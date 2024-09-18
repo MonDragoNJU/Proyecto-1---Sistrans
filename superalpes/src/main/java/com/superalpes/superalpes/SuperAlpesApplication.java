67e56e831417fdc8a5a5044f2c94e5298e31e75b
Julian Mondragon <123740082+MonDragoNJU@users.noreply.github.com>
2024-09-17 19:58:35 -0500

    Creacion de Esqueleto iwi
    SIUUUU

---
+++ superalpes/src/main/java/com/superalpes/superalpes/SuperAlpesApplication.java
@@ -0,0 +1,13 @@
+package com.superalpes.superalpes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperAlpesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperAlpesApplication.class, args);
+	}
+
+}
---
+++ superalpes/src/test/java/com/superalpes/superalpes/SuperAlpesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superalpes.superalpes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperAlpesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
